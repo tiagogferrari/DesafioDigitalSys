dc024d32763ac7f5d877bb69a80f9c0b64687197
Tiago Garcez Ferrari <tiagoggferrari@gmail.com>
2025-01-23 17:39:01 -0300

    Adicionando rotas

--- web/src/App.js
+++ web/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Auth from './components/Auth';
+import Curriculum from './pages/Curriculum';
+import Admin from './pages/Admin';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Auth />} />
+          <Route path="/curriculo" element={<Curriculum />} />
+          <Route path="/admin" element={<Admin />} />
         </Routes>
       </div>
     </Router>
